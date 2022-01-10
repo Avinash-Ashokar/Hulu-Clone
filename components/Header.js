bb6723cfd86a00873ba6f917ef2e202503fd66f0
Avinash-Ashokar <mail_avinashashokar@pm.me>
2022-01-11 00:10:09 +0530

    Completed Header

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,31 @@
 import Image from "next/image";
-import { HomeIcon } from "@heroicons/react/outline";
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 import HeaderItem from "./HeaderItem";
 
 export function Header() {
   return (
-    <div>
-      <HeaderItem title="HOME" Icon={HomeIcon} />
+    <header className="flex flex-col h-auto sm:flex-row justify-between m-5 items-center">
+      <div className="flex flex-grow justify-evenly max-w-2xl mr-2">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
       <Image
         className="object-container"
         src="/hulu-logo.png"
         width="200"
         height="100"
       />
-    </div>
+    </header>
   );
 }
--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -3,8 +3,7 @@ export default function HeaderItem({ Icon, title }) {
     <div className="group flex flex-col text-center w-12 sm:w-20 cursor-pointer hover:text-black">
       <Icon className="h-8 group-hover:animate-bounce mb-2" />
       <p className="opacity-0 group-hover:opacity-100 text-black font-bold tracking-wider">
-        {" "}
-        {title}{" "}
+        {title}
       </p>
     </div>
   );
